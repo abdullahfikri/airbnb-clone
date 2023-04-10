342654dda4649ebb78debe58aa1771982367ff7f
muhammad fikri <mfikri.dev@gmail.com>
2023-04-10 13:08:42 +0700

    Add login functionality
    Now user can login throught
    -Use email and password
    -Use github account
    -Use google account

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,20 +1,24 @@
 'use client';
 
-import axios from 'axios';
 import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
-import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
 import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
 import { toast } from 'react-hot-toast';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Button from '../Button';
 
-const RegisterModal = () => {
+const LoginModal = () => {
+    const router = useRouter();
     const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
     const [isLoading, setIsLoading] = useState(false);
 
     const {
@@ -23,7 +27,6 @@ const RegisterModal = () => {
         formState: { errors },
     } = useForm<FieldValues>({
         defaultValues: {
-            name: '',
             email: '',
             password: '',
         },
@@ -32,17 +35,22 @@ const RegisterModal = () => {
     const onSubmit: SubmitHandler<FieldValues> = (data) => {
         setIsLoading(true);
 
-        axios
-            .post('/api/register', data)
-            .then(() => {
-                registerModal.onClose();
-            })
-            .catch((error) => {
-                toast.error('Something went wrong.');
-            })
-            .finally(() => {
-                setIsLoading(false);
-            });
+        signIn('credentials', {
+            ...data,
+            redirect: false,
+        }).then((callback) => {
+            setIsLoading(false);
+
+            if (callback?.ok) {
+                toast.success('Logged in');
+                router.refresh();
+                loginModal.onClose();
+            }
+
+            if (callback?.error) {
+                toast.error(callback.error);
+            }
+        });
     };
 
     const bodyContent = (
@@ -56,14 +64,7 @@ const RegisterModal = () => {
                 errors={errors}
                 required
             />
-            <Input
-                id="name"
-                label="Name"
-                disable={isLoading}
-                register={register}
-                errors={errors}
-                required
-            />
+
             <Input
                 id="password"
                 label="password"
@@ -108,14 +109,14 @@ const RegisterModal = () => {
     return (
         <Modal
             disable={isLoading}
-            isOpen={registerModal.isOpen}
-            title="Register"
+            isOpen={loginModal.isOpen}
+            title="Login"
             actionLabel="Continue"
-            onClose={registerModal.onClose}
+            onClose={loginModal.onClose}
             onSubmit={handleSubmit(onSubmit)}
             body={bodyContent}
             footer={footerContent}
         />
     );
 };
-export default RegisterModal;
+export default LoginModal;
